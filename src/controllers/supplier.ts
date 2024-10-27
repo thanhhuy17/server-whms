4fecdd53d4f892129e870f31f51b53642de01c0e
Nguyen Thanh Huy <huymau170819999@gmail.com>
2024-10-27 14:40:39 +0700

    update cols supplier dynamic

--- src/controllers/supplier.ts
+++ src/controllers/supplier.ts
@@ -1,3 +1,4 @@
+import { supplierForm } from "../forms/supplier";
 import SupplierModel from "../models/SupplierModel"
 
 
@@ -89,91 +90,7 @@ const getFormSupplier = async (req: any, res: any) => {
             labelCol: 6,
             wrapperCol: 18,
             size: `middle`,
-            formItems: [
-                {
-                    key: `name`,
-                    value: `name`,
-                    require: true,
-                    message: `Please Enter Supplier Name!`,
-                    label: `Supplier Name`,
-                    placeholder: `Enter Supplier Name`,
-                    type: `input`
-
-                },
-                {
-                    key: `email`,
-                    value: `email`,
-                    require: true,
-                    message: `Please Enter Email!`,
-                    label: `Email`,
-                    placeholder: `Enter Supplier Email`,
-                    type: `input`,
-
-                },
-                {
-                    key: `product`,
-                    value: `product`,
-                    require: true,
-                    message: `Enter Product!`,
-                    label: `Product`,
-                    placeholder: `Enter Product`,
-                    type: `input`,
-
-                },
-                {
-                    key: `category`,
-                    value: `category`,
-                    // require: true,
-                    // message: `Enter Product!`,
-                    label: `Category`,
-                    placeholder: `Select product category`,
-                    type: `select`,
-
-                },
-                {
-                    key: `price`,
-                    value: `price`,
-                    // require: true,
-                    // message: `Enter Product!`,
-                    label: `Buying Price`,
-                    placeholder: `Enter buying price`,
-                    type: `input`,
-
-                },
-                {
-                    key: `contactNumber`,
-                    value: `contactNumber`,
-                    // require: true,
-                    // message: `Enter Product!`,
-                    label: `Contact Number`,
-                    placeholder: `Enter supplier contact number`,
-                    type: `input`,
-
-                },
-                {
-                    key: `active`,
-                    value: `active`,
-                    // require: true,
-                    // message: `Enter Product!`,
-                    label: `Active`,
-                    placeholder: `Enter Active number`,
-                    type: `number`,
-                    // typeInput: `number`,
-
-
-                },
-                {
-                    key: `type`,
-                    value: `type`, //name
-                    // require: true,
-                    // message: `Enter Product!`,
-                    label: `Taking Return`,
-                    // placeholder: `Enter Active number`,
-                    type: `checkbox`,
-
-                },
-
-            ]
+            formItems: supplierForm
         }
         res.status(200).json({
             message: `Get Form Success`,
---
+++ src/forms/supplier.ts
@@ -0,0 +1,93 @@
+// ------------ SUPPLIER FORM --------------
+export const supplierForm = [
+    {
+        key: `name`,
+        value: `name`,
+        require: true,
+        message: `Please Enter Supplier Name!`,
+        label: `Supplier Name`,
+        placeholder: `Enter Supplier Name`,
+        type: `input`
+
+    },
+    {
+        key: `email`,
+        value: `email`,
+        require: true,
+        message: `Please Enter Email!`,
+        label: `Email`,
+        placeholder: `Enter Supplier Email`,
+        type: `input`,
+
+    },
+    {
+        key: `product`,
+        value: `product`,
+        require: true,
+        message: `Enter Product!`,
+        label: `Product`,
+        placeholder: `Enter Product`,
+        type: `input`,
+
+    },
+    {
+        key: `category`,
+        value: `category`,
+        // require: true,
+        // message: `Enter Product!`,
+        label: `Category`,
+        placeholder: `Select product category`,
+        type: `select`,
+
+    },
+    {
+        key: `price`,
+        value: `price`,
+        // require: true,
+        // message: `Enter Product!`,
+        label: `Buying Price`,
+        placeholder: `Enter buying price`,
+        type: `input`,
+
+    },
+    {
+        key: `contactNumber`,
+        value: `contactNumber`,
+        // require: true,
+        // message: `Enter Product!`,
+        label: `Contact Number`,
+        placeholder: `Enter supplier contact number`,
+        type: `input`,
+
+    },
+    {
+        key: `active`,
+        value: `active`,
+        // require: true,
+        // message: `Enter Product!`,
+        label: `Active`,
+        placeholder: `Enter Active number`,
+        type: `number`,
+        // typeInput: `number`,
+
+
+    },
+    {
+        key: `type`,
+        value: `type`, //name
+        // require: true,
+        // message: `Enter Product!`,
+        label: `Taking Return`,
+        // placeholder: `Enter Active number`,
+        type: `checkbox`,
+
+    },
+
+]
+
+// ------------ SUPPLIER COLUMN TABLE --------------
+// export const supplierCol = [
+//     {
+
+//     }
+// ]
