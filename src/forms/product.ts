4eaf0688be275071d77308f4bc40d5a3ac0f0b85
Nguyen Thanh Huy <huymau170819999@gmail.com>
2024-11-04 00:06:49 +0700

    update product form

--- src/forms/product.ts
+++ src/forms/product.ts
@@ -14,7 +14,7 @@ export const productForm = {
             require: true,
             message: `Please Enter Product Name!`,
             label: `Product Name`,
-            placeholder: `Enter Product Name`,
+            placeholder: `Enter product name`,
             type: `input`,
             displayLength: 160
 
@@ -25,7 +25,7 @@ export const productForm = {
             require: true,
             message: `Please Enter Product Id!`,
             label: `Product Id`,
-            placeholder: `Enter Product Id`,
+            placeholder: `Enter product Id`,
             type: `input`,
             displayLength: 160
 
@@ -33,10 +33,67 @@ export const productForm = {
         {
             key: `category`,
             value: `category`,
-            require: true,
-            message: `Please Enter Category!`,
+            // require: true,
+            message: `Please Select Category!`,
             label: `Category`,
-            placeholder: `Enter Category`,
+            placeholder: `Select product category`,
+            type: `select`,
+            displayLength: 160
+
+        },
+        {
+            key: `buyingPrice`,
+            value: `buyingPrice`,
+            require: true,
+            message: `Please Enter Buying Price!`,
+            label: `Buying Price`,
+            placeholder: `Enter buying price`,
+            type: `input`,
+            displayLength: 160
+
+        },
+        {
+            key: `quantity`,
+            value: `quantity`,
+            require: true,
+            message: `Please Enter Quantity!`,
+            label: `Quantity`,
+            placeholder: `Enter product quantity`,
+            type: `input`,
+            displayLength: 160
+
+        },
+        {
+            key: `unit`,
+            value: `unit`,
+            require: true,
+            message: `Please Enter Unit!`,
+            label: `Unit`,
+            placeholder: `Enter product unit`,
+            type: `input`,
+            displayLength: 160
+
+        },
+        {
+            // Chỉnh lại
+            key: `expiryDate`,
+            value: `expiryDate`,
+            require: true,
+            message: `Please Enter Expiry Date!`,
+            label: `Expiry Date`,
+            placeholder: `Enter expiry date`,
+            type: `input`,
+            displayLength: 160
+
+        },
+        {
+            // Chỉnh lại
+            key: `thresholdValue`,
+            value: `thresholdValue`,
+            require: true,
+            message: `Please Enter Threshold Value!`,
+            label: `Threshold Value`,
+            placeholder: `Enter threshold value`,
             type: `input`,
             displayLength: 160
 
